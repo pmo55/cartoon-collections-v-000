817427a74f5cf066d337945754ee7dce93e9075f
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-03 02:02:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese.find do |cheese|
+  array.find do |cheese|
     cheese_types.include?(cheese)
   end
 end
